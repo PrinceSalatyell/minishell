c3297d6013122e4c953370242f1843f5397d9400
Tiago <tiagofmpereira22@gmail.com>
2023-03-26 17:47:52 +0100

    Save some changes

--- parser.c
+++ parser.c
@@ -6,35 +6,24 @@
 /*   By: josanton <josanton@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/22 21:06:02 by josanton          #+#    #+#             */
-/*   Updated: 2023/03/22 21:47:02 by josanton         ###   ########.fr       */
+/*   Updated: 2023/03/26 12:06:13 by josanton         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	tokenizer(char *str, int i)
+void	parse_commands(t_token *token_lst)
 {
-	int	len;
-	int	tk_len;
+	int	nr_commands;
 
-	len = matrix_len(str);
-	_input()->token_matrix = malloc(sizeof(char *) * (len + 1));
-	if (!_input()->token_matrix)
-		return ;
-	_input()->token_matrix[len] = '\0';
-	_input()->index = 0;
-	while (str[i])
+	nr_commands = 0;
+	if (token_lst)
 	{
 		while (token_lst->next)
 		{
-			tk_len = token_len(str, i);
-			_input()->token_matrix[_input()->index] = \
-			malloc(sizeof(char *) * (tk_len + 1));
-			ft_strncpy(_input()->token_matrix[_input()->index], \
-			str + i, tk_len);
-			_input()->token_matrix[_input()->index][tk_len] = '\0';
-			_input()->index++;
-			i = i + tk_len;
+			if (ft_strcmp(token_lst->type, "Operator") == 0)
+				nr_commands++;
+			token_lst = token_lst->next;
 		}
 	}
 	printf("nr_commands - %d\n", nr_commands);
